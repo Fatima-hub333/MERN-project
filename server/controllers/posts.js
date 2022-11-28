bffd3523deb59856b528e0412537db5062acfb05
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-28 12:30:43 +0500

    update forms card logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,7 +29,6 @@ export const getPost = async (req, res) => {
 
 export const createPost = async (req, res) => {
     const { title, message, selectedFile, creator, tags } = req.body;
-
     const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
 
     try {
@@ -44,13 +43,10 @@ export const createPost = async (req, res) => {
 export const updatePost = async (req, res) => {
     const { id } = req.params;
     const { title, message, creator, selectedFile, tags } = req.body;
-    
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-
     const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
     await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
-
     res.json(updatedPost);
 }
 
