f14244188ae02eca0ee2cec96e1d3b99ccf22990
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-28 14:17:07 +0500

    create .env file

--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-import exppress from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 
-const app = exppress();
+const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-// https://www.mongodb.com/cloud/atlas
-// const CONNECTION_URL = 'mongodb+srv://fatima:1212@cluster0.gxn5wto.mongodb.net/?retryWrites=true&w=majority';
+
+const CONNECTION_URL = 'mongodb+srv://fatima:1212@cluster0.gxn5wto.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
