a299a28a1ff39af6cb616f7bc1245f283f278aaa
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-11-28 13:56:44 +0500

    update connection url

--- server/index.js
+++ server/index.js
@@ -12,11 +12,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 // https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = 'mongodb+srv://fatima:1212@cluster0.gxn5wto.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb+srv://fatima:1212@cluster0.gxn5wto.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-// mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);
